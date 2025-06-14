2c5370592c6772c2b87050483a6f8937814ded23
imabeastdrew <drewtaylor635@gmail.com>
2025-06-14 15:22:28 -0500

    Update setup.py to match requirements.txt and use system versions of numpy and wandb

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "torch>=2.1.0",
-        "numpy>=1.24.0",
-        "wandb>=0.15.12",
+        "torch==2.5.1",
+        "torchvision==0.20.1",
+        "torchaudio==2.5.1",
+        "numpy",
+        "wandb",
         "tqdm>=4.65.0",
         "pandas>=2.0.0",
+        "pathlib>=1.0.1",
     ],
     python_requires=">=3.8",
 )
