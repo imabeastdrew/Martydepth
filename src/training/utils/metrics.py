4990780557aa3f4b4e49a19af7bbf93b109cd22f
imabeastdrew <drewtaylor635@gmail.com>
2025-06-15 12:10:13 -0500

    updated GPU memory metrics for better monitoring

--- src/training/utils/metrics.py
+++ src/training/utils/metrics.py
@@ -28,34 +28,38 @@ def log_training_metrics(model: torch.nn.Module,
                         optimizer: torch.optim.Optimizer,
                         epoch: int,
                         step: int):
-    """Log basic training metrics"""
-    # Training metrics
+    """Log training metrics to wandb"""
+    # Get learning rate
+    lr = optimizer.param_groups[0]['lr']
+    
+    # Calculate gradient norm
+    total_norm = 0.0
+    for p in model.parameters():
+        if p.grad is not None:
+            param_norm = p.grad.detach().data.norm(2)
+            total_norm += param_norm.item() ** 2
+    total_norm = total_norm ** 0.5
+    
+    # Calculate parameter norm
+    param_norm = 0.0
+    for p in model.parameters():
+        param_norm += p.detach().data.norm(2).item() ** 2
+    param_norm = param_norm ** 0.5
+    
+    # Log metrics
     metrics = {
         'train/loss': loss.item(),
-        'train/learning_rate': optimizer.param_groups[0]['lr'],
+        'train/learning_rate': lr,
         'train/epoch': epoch,
+        'train/grad_norm': total_norm,
+        'train/param_norm': param_norm,
     }
     
-    # Gradient norms (for plotting)
-    grad_norms = {
-        f'gradients/layer_{i}_norm': p.grad.norm().item()
-        for i, p in enumerate(model.parameters())
-        if p.grad is not None
-    }
-    metrics.update(grad_norms)
-    
-    # Parameter norms (for plotting)
-    param_norms = {
-        f'parameters/layer_{i}_norm': p.norm().item()
-        for i, p in enumerate(model.parameters())
-    }
-    metrics.update(param_norms)
-    
-    # Hardware metrics
+    # Add basic GPU memory metrics if available
     if torch.cuda.is_available():
         metrics.update({
-            'system/gpu_memory_gb': torch.cuda.memory_allocated() / 1024**3,
-            'system/gpu_utilization': torch.cuda.utilization(),
+            'system/gpu_memory_allocated': torch.cuda.memory_allocated() / 1e9,  # GB
+            'system/gpu_memory_reserved': torch.cuda.max_memory_reserved() / 1e9,  # GB
         })
     
     wandb.log(metrics, step=step)
